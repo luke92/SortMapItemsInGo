affefa8242f0b486216d5aa37936e3ef1f6c3349
luke92 <lucasjv92@gmail.com>
2022-06-21 19:19:54 -0300

    Upload project

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/luke92/SortMapItemsInGo
+
+go 1.18
---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+type DiscordUser struct {
+	DiscordUserID string            `json:"id"`
+	VotingPhases  map[string]uint32 `json:"voting_phases"`
+}
+
+func main() {
+
+	discordUsers := []DiscordUser{}
+	discordUsers = addUser("3", uint32(20), discordUsers)
+	discordUsers = addUser("1", uint32(60), discordUsers)
+	discordUsers = addUser("4", uint32(10), discordUsers)
+	discordUsers = addUser("3", uint32(40), discordUsers)
+
+	sort.Slice(discordUsers[:], func(i, j int) bool {
+		return discordUsers[i].VotingPhases["phase_1"] > discordUsers[j].VotingPhases["phase_1"]
+	})
+
+	fmt.Println(discordUsers)
+}
+
+func addUser(userId string, points uint32, discordUsers []DiscordUser) []DiscordUser {
+	discordUser := DiscordUser{
+		DiscordUserID: userId,
+		VotingPhases: map[string]uint32{
+			"phase_1": points,
+		},
+	}
+
+	discordUsers = append(discordUsers, discordUser)
+	return discordUsers
+}
